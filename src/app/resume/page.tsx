c20d5f62aaa9f54133f8b7c45f5e9b0aa55e0763
Abhushan Gautam <carefreeav09@gmail.com>
2023-08-02 00:16:33 +0545

    fix: added gradiant BG to resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -33,119 +33,121 @@ const Timeline: React.FC<{
 
 const Resume = () => {
   return (
-    <div className="min-h-screen h-full flex justify-between items-start px-32 pt-32">
-      {/* Work history column */}
+    <PageWrapper>
+      <div className="min-h-screen h-full flex justify-between items-start px-32 pt-32 bg-gradient-to-b from-[#19197030] to-[#19197040]">
+        {/* Work history column */}
 
-      <div className="flex-1">
-        <div className="flex items-center justify-start gap-8">
-          <div className="text-3xl rounded-full bg-indigo-800 p-4">
-            <HiBookmark />
+        <div className="flex-1">
+          <div className="flex items-center justify-start gap-8">
+            <div className="text-3xl rounded-full bg-indigo-800 p-4">
+              <HiBookmark />
+            </div>
+
+            <p className="text-3xl font-semibold">work history</p>
           </div>
 
-          <p className="text-3xl font-semibold">work history</p>
-        </div>
+          {/* side line */}
 
-        {/* side line */}
+          <div className="w-full h-full mx-8 border-l-2 border-gray-900 mb-10">
+            <div className="relative pt-10">
+              <Timeline startDate="2020" current />
+              <div className="pt-10 mx-12">
+                <p className="font-bold text-2xl -tracking-tighter mb-2">
+                  FullStack Web Developer
+                </p>
+                <p className="font-bolder text-gray-400 uppercase mb-6">
+                  Kotuko Nepal
+                </p>
 
-        <div className="w-full h-full mx-8 border-l-2 border-gray-900 mb-10">
-          <div className="relative pt-10">
-            <Timeline startDate="2020" current />
-            <div className="pt-10 mx-12">
-              <p className="font-bold text-2xl -tracking-tighter mb-2">
-                FullStack Web Developer
-              </p>
-              <p className="font-bolder text-gray-400 uppercase mb-6">
-                Kotuko Nepal
-              </p>
+                <p className="-tracking-tight font-light text-lg">
+                  I currently work for Kotuko Nepal, a regional branch of
+                  multi-national organizaion, Kotuko IT. I primarily work as a
+                  frontend lead, mentor, creating clean code structure and
+                  reusable layouts.
+                </p>
+              </div>
+            </div>
 
-              <p className="-tracking-tight font-light text-lg">
-                I currently work for Kotuko Nepal, a regional branch of
-                multi-national organizaion, Kotuko IT. I primarily work as a
-                frontend lead, mentor, creating clean code structure and
-                reusable layouts.
-              </p>
+            <div className="relative pt-10">
+              <Timeline startDate="2018" endDate="2020" />
+
+              <div className="pt-10 mx-12">
+                <p className="font-bold text-2xl -tracking-tighter mb-2">
+                  Associate React JS Developer
+                </p>
+                <p className="font-bolder text-gray-400 uppercase mb-6">
+                  Citytech IT, Nepal
+                </p>
+
+                <p className="-tracking-tight font-light text-lg">
+                  I worked as a React JS developer, building multiple fintech
+                  applications with React and NodeJS. I encouraged TDD while
+                  developing applications.
+                </p>
+              </div>
+            </div>
+
+            <div className="relative pt-10">
+              <Timeline startDate="2018" endDate="2020" />
+
+              <div className="pt-10 mx-12">
+                <p className="font-bold text-2xl -tracking-tighter mb-2">
+                  Frontend Developer
+                </p>
+                <p className="font-bolder text-gray-400 uppercase mb-6">
+                  National Incubation and Research Center, Nepal
+                </p>
+
+                <p className="-tracking-tight font-light text-lg">
+                  I worked as a frontend developer, working with HTML5, CSS and
+                  Jquery as i was starting my journey. I worked on multiple
+                  projects ranging from food delivery to government application.
+                </p>
+              </div>
             </div>
           </div>
 
-          <div className="relative pt-10">
-            <Timeline startDate="2018" endDate="2020" />
+          <div className="flex items-center justify-start gap-8">
+            <div className="text-3xl rounded-full bg-indigo-800 p-4">
+              <MdSchool />
+            </div>
 
-            <div className="pt-10 mx-12">
-              <p className="font-bold text-2xl -tracking-tighter mb-2">
-                Associate React JS Developer
-              </p>
-              <p className="font-bolder text-gray-400 uppercase mb-6">
-                Citytech IT, Nepal
-              </p>
+            <p className="text-3xl font-semibold">education</p>
+          </div>
 
-              <p className="-tracking-tight font-light text-lg">
-                I worked as a React JS developer, building multiple fintech
-                applications with React and NodeJS. I encouraged TDD while
-                developing applications.
-              </p>
+          {/* side line */}
+
+          <div className="w-full h-full mx-8 border-l-2 border-gray-900">
+            <div className="relative pt-10">
+              <Timeline startDate="2013" endDate="2017" />
+              <div className="pt-10 mx-12">
+                <p className="font-bold text-2xl -tracking-tighter mb-2">
+                  BSC. CSIT
+                </p>
+                <p className="font-bolder text-gray-400 uppercase mb-6">
+                  Deerwalk Institue of Technology
+                </p>
+              </div>
             </div>
           </div>
 
-          <div className="relative pt-10">
-            <Timeline startDate="2018" endDate="2020" />
+          {/* Button */}
 
-            <div className="pt-10 mx-12">
-              <p className="font-bold text-2xl -tracking-tighter mb-2">
-                Frontend Developer
-              </p>
-              <p className="font-bolder text-gray-400 uppercase mb-6">
-                National Incubation and Research Center, Nepal
-              </p>
-
-              <p className="-tracking-tight font-light text-lg">
-                I worked as a frontend developer, working with HTML5, CSS and
-                Jquery as i was starting my journey. I worked on multiple
-                projects ranging from food delivery to government application.
-              </p>
-            </div>
-          </div>
+          <motion.div
+            className="flex items-center justify-center px-6 py-4 border-4 rounded-md border-gray-800 max-w-xs my-10 text-lg gap-4 cursor-pointer"
+            whileTap={{
+              scale: 0.9,
+            }}
+          >
+            <HiDownload />
+            <p className="text-bold text-xl lowercase">Download CV</p>
+          </motion.div>
         </div>
-
-        <div className="flex items-center justify-start gap-8">
-          <div className="text-3xl rounded-full bg-indigo-800 p-4">
-            <MdSchool />
-          </div>
-
-          <p className="text-3xl font-semibold">education</p>
+        <div className="flex-[0.5]">
+          <Ribbon title="skills" />
         </div>
-
-        {/* side line */}
-
-        <div className="w-full h-full mx-8 border-l-2 border-gray-900">
-          <div className="relative pt-10">
-            <Timeline startDate="2013" endDate="2017" />
-            <div className="pt-10 mx-12">
-              <p className="font-bold text-2xl -tracking-tighter mb-2">
-                BSC. CSIT
-              </p>
-              <p className="font-bolder text-gray-400 uppercase mb-6">
-                Deerwalk Institue of Technology
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* Button */}
-
-        <motion.div
-          className="flex items-center justify-center px-6 py-4 border-4 rounded-md border-gray-800 max-w-xs my-10 text-lg gap-4 cursor-pointer"
-          whileTap={{
-            scale: 0.9,
-          }}
-        >
-          <HiDownload />
-          <p className="text-bold text-xl lowercase">Download CV</p>
-        </motion.div>
       </div>
-      <div className="flex-[0.5]">
-        <Ribbon title="skills" />
-      </div>
-    </div>
+    </PageWrapper>
   );
 };
 
