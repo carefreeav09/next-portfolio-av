311e0539b6eeedd78444fa64f8953170191eeb12
Abhushan Gautam <carefreeav09@gmail.com>
2023-08-07 21:18:59 +0545

    fix: contact page has new content

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,16 @@
+import PageWrapper from "@/components/@app/PageWrapper";
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <PageWrapper>
+      <div className="h-screen w-full flex items-center justify-center">
+        <p className="text-4xl tracking-tighter font-bold">
+          Blog in progress....
+        </p>
+      </div>
+    </PageWrapper>
+  );
 };
 
 export default Blog;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,66 @@ import React from "react";
 const Contact = () => {
   return (
     <PageWrapper>
-      <p className="text-7xl">Contact me</p>
+      <div className="h-screen w-10/12 flex justify-between items-center gap-10 m-auto">
+        <div className="">
+          <p className="text-xl tracking-tight text-gray-500 mb-0">
+            have an idea in mind?
+          </p>
+
+          <p className="text-4xl tracking-tighter mb-10"> Send me a message</p>
+
+          <form className="">
+            <div className="grid grid-cols-1 gap-y-4">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="tracking-tighter text-sm text-gray-500"
+                >
+                  email
+                </label>
+                <input
+                  type="text"
+                  className="w-full  px-2 py-2 rounded-md outline-none bg-slate-600 text-white"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="tracking-tighter text-sm text-gray-500"
+                >
+                  subject
+                </label>
+                <input
+                  type="text"
+                  className="w-full  px-2 py-2 rounded-md outline-none  bg-slate-600 text-white"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="tracking-tighter text-sm text-gray-500"
+                >
+                  message
+                </label>
+
+                <textarea
+                  className="w-full  px-2 py-2 rounded-md outline-none bg-slate-600 text-white"
+                  rows={2}
+                />
+              </div>
+              <button
+                type="submit"
+                className="p-4 bg-indigo-800 text-white rounded-lg hover:bg-indigo-600 font-bold shadow-md shadow-indigo-600"
+              >
+                Lets connect
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className="justify-self-end">looking for google maps API lol</div>
+      </div>
     </PageWrapper>
   );
 };
