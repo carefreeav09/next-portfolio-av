0ad3c4f7f25022d353d78e1d9591fd89cf9a0844
Aabhushan Gautam <carefreeav09@gmail.com>
2023-11-03 20:26:12 +0545

    fix: updated gitignore to ingore the env file

--- src/utils/axios.utils.ts
+++ src/utils/axios.utils.ts
@@ -1,28 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 
-
-const baseUrl = process.env.NEXT_PUBLIC_API_URL + "api/v1";
+const baseUrl = process.env.NEXT_PUBLIC_API_URL + 'api/v1';
 
 /**
  *
  */
 const axiosInstance = axios.create({
-    headers: {
-        "Content-Type": "application/json",
-    },
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Adding Auhorization header to every request if token is available
 axiosInstance.interceptors.request.use((config) => {
-    // const token = store.getState().auth.token;
-    const token = "";
+  // const token = store.getState().auth.token;
+  const token = '';
 
-    //
-    if (config.headers && token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+  //
+  if (config.headers && token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
+  return config;
 });
 
 //
